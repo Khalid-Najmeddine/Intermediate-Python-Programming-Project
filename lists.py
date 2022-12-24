a5bf8308d149b818153202465efb7ddb966fc6a2
Khalid Najmeddine <knajm574@gmail.com>
2022-12-24 03:09:53 -0700

    Feat: Lists

---
+++ lists.py
@@ -0,0 +1,22 @@
+#Lists: ordered, mutable, allows duplicate elements
+
+firstList = ["banana", "cherry", "apple"]
+print(firstList)
+
+secondList = list() 
+print(secondList)
+
+alteredSecondList = [5, True, "apple", "apple"]
+print(alteredSecondList)
+
+item = firstList[0]
+print(item)
+
+item = firstList[-1]
+print(item)
+
+# item = firstList[3]
+# print(item)
+
+for i in firstList: 
+  print(i)
